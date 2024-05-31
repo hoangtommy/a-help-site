5477e8b8719927c258f82312be017566322bb138
Tommy <hoang.tommy12@gmail.com>
2024-05-31 07:19:51 -0700

    added accredd img and updated config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,22 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Accredd Info & Help',
+  tagline: 'Modern Investor Accreditations',
+  favicon: 'img/favicon-accredd.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://info.accredd.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hoangtommy', // Usually your GitHub org/user name.
+  projectName: 'a-help-site', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -41,17 +43,18 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          // routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://accredd.com',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://accredd.com',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -66,22 +69,22 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Accredd Info & Help',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Accredd Logo',
+          src: 'img/logo-accredd.svg',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Info & Help',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://accredd.com',
+            label: 'Accredd Home',
             position: 'right',
           },
         ],
@@ -90,28 +93,32 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Info',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Help Articles',
+                to: '/',
+              },
+              {
+                label: 'Info',
+                to: 'https://api.accredd.com',
               },
             ],
           },
           {
-            title: 'Community',
+            title: 'Support',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Investors',
+                to: 'https://accredd.com',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'Issuers',
+                to: 'https://accredd.com',
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'support@accredd.com',
+                to: 'mailto:support@accredd.com',
               },
             ],
           },
@@ -123,13 +130,13 @@ const config = {
                 to: '/blog',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Home',
+                href: 'https://accredd.com',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Accredd, Inc.`,
       },
       prism: {
         theme: prismThemes.github,
